1f547f014f739c475341c145e30bfd2fe1aa4387
Serhii Hatsan <shatsanik01@office365.fiot.kpi.ua>
2023-02-15 01:32:46 +0200

    feat(Lab1): Complete Lab1

--- Lab1/src.py
+++ Lab1/src.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+import re
+
 file_text = ""
 
 with open("text3.txt", "r", encoding="utf-8") as file:
     file_text = file.read()
 
 def task_1():
+    print("Task 1")
+    print("----------------------------------")
     text_splice = file_text[0:20]
     print("Splice: " + text_splice)
     print("Splice length: " + str(len(text_splice)))
@@ -18,7 +22,44 @@ def task_1():
     print("Join previously splitted strings: " + "".join(text_splice.split(' ')))
 
 def task_2():
-    pass
+    global file_text
+    print("----------------------------------")
+    print("Task 2")
+    print("Variant 11")
+    print("----------------------------------")
+    print("Whole file content:")
+    print(file_text)
+    print("Preparing regex...")
+    print("----------------------------------")
+    n = re.compile(r'(?<=(?<=^)|(?<=[^\w(]))\+?\(?\d{2,3}\)?(?:\-\d{1,3}){2,3}(?=(?<=$)|(?=[^\w-]))')
+    print("Blurring phone numbers...")
+    start = 0
+    end = len(file_text)
+
+    while start != end:
+        res = n.search(file_text, start, end)
+
+        if res == None:
+            break
+
+        print(res)
+        startPos = res.span()[0]
+        endPos = res.span()[1]
+        numCount = 0
+        #"""
+        while startPos != endPos:
+            if file_text[startPos].isnumeric():
+                if numCount == 2:
+                    file_text = file_text[:startPos] + 'X' + file_text[startPos+1:]
+                else:
+                    numCount = numCount + 1
+            startPos = startPos + 1
+        #"""
+        start = endPos
+
+    print("Done.")
+    print("----------------------------------")
+    print(file_text)
 
 if __name__ == "__main__":
     task_1()
