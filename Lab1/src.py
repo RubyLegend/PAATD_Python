37f9261ee5421c35426e0975c9b581333bfbb787
Serhii Hatsan <shatsanik01@office365.fiot.kpi.ua>
2023-03-15 01:47:34 +0200

    feat(Lab2): Initial commit

--- Lab1/src.py
+++ Lab1/src.py
@@ -6,20 +6,24 @@ file_text = ""
 with open("text3.txt", "r", encoding="utf-8") as file:
     file_text = file.read()
 
+
 def task_1():
     print("Task 1")
     print("----------------------------------")
     text_splice = file_text[0:20]
     print("Splice: " + text_splice)
     print("Splice length: " + str(len(text_splice)))
-    print("Number of a's in text: " + str(text_splice.count('a')))
-    find_res = text_splice.find('q')
-    print("Position of 'q' in text: " + str(find_res if find_res != -1 else "Not found"))
+    print("Number of a's in text: " + str(text_splice.count("a")))
+    find_res = text_splice.find("q")
+    print(
+        "Position of 'q' in text: " + str(find_res if find_res != -1 else "Not found")
+    )
     print("Upper case: " + text_splice.upper())
     print("Replaced 'Slowly' with 'Fastly': " + text_splice.replace("Slowly", "Fastly"))
-    print("Split string by symbol ' ': ", end='')
-    print(text_splice.split(' '))
-    print("Join previously splitted strings: " + "".join(text_splice.split(' ')))
+    print("Split string by symbol ' ': ", end="")
+    print(text_splice.split(" "))
+    print("Join previously splitted strings: " + "".join(text_splice.split(" ")))
+
 
 def task_2():
     global file_text
@@ -31,7 +35,9 @@ def task_2():
     print(file_text)
     print("Preparing regex...")
     print("----------------------------------")
-    n = re.compile(r'(?<=(?<=^)|(?<=[^\w(]))\+?\(?\d{2,3}\)?(?:\-\d{1,3}){2,3}(?=(?<=$)|(?=[^\w-]))')
+    n = re.compile(
+        r"(?<=(?<=^)|(?<=[^\w(]))\+?\(?\d{2,3}\)?(?:\-\d{1,3}){2,3}(?=(?<=$)|(?=[^\w-]))"
+    )
     print("Blurring phone numbers...")
     start = 0
     end = len(file_text)
@@ -39,7 +45,7 @@ def task_2():
     while start != end:
         res = n.search(file_text, start, end)
 
-        if res == None:
+        if res is None:
             break
 
         startPos = res.span()[0]
@@ -49,7 +55,7 @@ def task_2():
         while startPos != endPos:
             if file_text[startPos].isnumeric():
                 if numCount == 2:
-                    file_text = file_text[:startPos] + 'X' + file_text[startPos+1:]
+                    file_text = file_text[:startPos] + "X" + file_text[startPos + 1:]
                 else:
                     numCount = numCount + 1
             startPos = startPos + 1
@@ -59,6 +65,7 @@ def task_2():
     print("Done.")
     print("----------------------------------")
     print(file_text)
+
 
 if __name__ == "__main__":
     task_1()
---
+++ Lab2/src.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+from sty import fg, rs
+import colorsys
+
+import re
+
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import brown
+
+# nltk.download('all', download_dir="./nltk_data")
+nltk.data.path.append("./nltk_data")
+
+
+def h_to_color(h):
+    (r, g, b) = colorsys.hsv_to_rgb(h, 1, 1)
+    (r, g, b) = (int(r * 255), int(g * 255), int(b * 255))
+    return fg(r, g, b)
+
+
+if __name__ == "__main__":
+    with open("text1.txt") as file:
+        text = file.read()
+        print(text, end="\n---------------------\n")
+
+        print("Кількість речень в тексті: " + str(len(sent_tokenize(text))))
+
+        number = 1
+        for sent in sent_tokenize(text):
+            print(str(number) + ". " + sent, end="\n\n")
+            number += 1
+
+        pos_tags = dict()
+        for item in nltk.pos_tag(word_tokenize(text)):
+            try:
+                pos_tags[item[1]] += 1
+            except KeyError:
+                pos_tags[item[1]] = 1
+
+        pos_tags_len = len(pos_tags)
+        list_pos_tags = list(pos_tags)
+
+        print("Color guide:")
+        for tag_index in range(len(list_pos_tags)):
+            print(
+                h_to_color(tag_index / pos_tags_len) + list_pos_tags[tag_index] + fg.rs,
+                end='\t'
+            )
+
+        print('\n')
+        for word, tag in nltk.pos_tag(word_tokenize(text)):
+            index = list_pos_tags.index(tag)
+            print(h_to_color(index / pos_tags_len) + word + fg.rs + " ", end="")
+
+        print("\n")
+
+        print("Частини мови: " + str(nltk.pos_tag(word_tokenize(text))), end="\n\n")
+
+        freqDist = nltk.FreqDist(word_tokenize(text))
+        print("Частота зустрічі слів: " + str(freqDist.most_common(10)), end="\n\n")
+
+        print("---------------------\n")
+        print(
+            "Кількість слів в категорії science_fiction: "
+            + str(len(brown.words(categories="science_fiction"))),
+            end="\n\n",
+        )
+
+        # print(brown.fileids(categories='science_fiction'))
+        sents = brown.tagged_words(fileids="cm02")
+        # print(sents)
+        regex = re.compile(r"^V.*")
+        # print(len(' '.join([''.join(sent_tokens) for sent_tokens, tags in sents])))
+        # print(len(' '.join([''.join(sent_tokens) for sent_tokens, tags in sents if not regex.search(tags)])))
+        print("Другий текст без дієслів: ")
+        print(
+            " ".join(
+                [
+                    "".join(sent_tokens)
+                    for sent_tokens, tags in sents
+                    if not regex.search(tags)
+                ]
+            )
+        )
