b94f4a7bd6fb29f5025437555d2f90a53e81d8de
Serhii Hatsan <shatsanik01@office365.fiot.kpi.ua>
2023-03-29 11:34:01 +0300

    feat(Lab3): Rewrite prepare part, error protection

--- Lab3/main.py
+++ Lab3/main.py
@@ -12,21 +12,39 @@ from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 
 import nltk
 from nltk.tokenize import WordPunctTokenizer
+from nltk.corpus import stopwords
 from gensim.models import word2vec
 
 nltk.data.path.append("../Lab2/nltk_data")
 
 file = open("doc11.txt", 'r').read().split('\n')
-file = [sentence for sentence in file if len(sentence) != 0]
+wpt = nltk.WordPunctTokenizer()
 
-corpus = [re.sub(r'[^a-zA-Z\s]', '', sentence, re.I | re.A).lower() for sentence in file]
+# Preparing corpus
+corpus = [sentence for sentence in file if len(sentence) != 0]
+
+
+def preprocess_sentence(sentence):
+    sentence = re.sub(r'[^a-zA-Z\s]', '', sentence, re.I | re.A)
+    sentence = sentence.lower()
+    sentence = sentence.strip()
+    tokens = wpt.tokenize(sentence)
+    filtered_tokens = [token for token in tokens if token not in stopwords.words('english')]
+    sentence = ' '.join(filtered_tokens)
+    return sentence
+
+
+prepared_corpus = []
+
+for sentence in corpus:
+    prepared_corpus.append(preprocess_sentence(sentence))
 
 print("Підготовлений корпус: ")
-print(corpus)
+print(prepared_corpus)
 
 # Task 1
 cv = CountVectorizer(min_df=0., max_df=1.)
-cv_matrix = cv.fit_transform(corpus)
+cv_matrix = cv.fit_transform(prepared_corpus)
 vocab = cv.get_feature_names_out()
 
 array = pd.DataFrame(cv_matrix.toarray(), columns=vocab)
@@ -63,38 +81,38 @@ dendrogram(links)
 plt.show()
 
 # From dendrogram we can choose max distance = 1.5
-max_dist = 1.5
+max_dist = 1.6
 
 cluster_labels = fcluster(links, max_dist, criterion='distance')
 
-array4 = pd.DataFrame(corpus, columns=["Sentences"])
+array4 = pd.DataFrame(prepared_corpus, columns=["Sentences"])
 array5 = pd.DataFrame(cluster_labels, columns=["Cluster Labels ID"])
 array6 = pd.concat([array4, array5], axis=1)
 print(array6)
 print("\n\n")
 # --------
 # Task 3
-wpt = nltk.WordPunctTokenizer()
-tokenized_corpus = [wpt.tokenize(sentence) for sentence in corpus]
+tokenized_prepared_corpus = [wpt.tokenize(sentence) for sentence in prepared_corpus]
 vector_size = 100
 window = 30
 min_count = 1
 sample = 1e-3
-w2v_model = word2vec.Word2Vec(tokenized_corpus,
+w2v_model = word2vec.Word2Vec(tokenized_prepared_corpus,
                               vector_size=vector_size,
                               window=window,
                               min_count=min_count,
                               sample=sample)
 # Similar words
-sim_words = {search_term: [
+try:
+    for search_term in ["mobile", "athens"]:
+        print({search_term: [
                             item[0]
                             for item
                             in w2v_model.wv.most_similar(
                                                          [search_term],
                                                          topn=5
                                                         )
-                            ]
-              for search_term in ["mobile", "athens"]}
-
-print(sim_words)
+                            ]})
+except KeyError as e:
+    print(e)
 # --------
