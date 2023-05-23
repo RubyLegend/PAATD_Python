315f6a39cd532cbb47ad268bd04b5a970b00798f
Serhii Hatsan <shatsanik01@office365.fiot.kpi.ua>
2023-05-24 01:38:54 +0300

    feat(Lab5): Rewritten task 1

--- Lab5/main.py
+++ Lab5/main.py
@@ -5,6 +5,8 @@ import csv
 import numpy as np
 import pandas as pd
 
+from matplotlib import pyplot as plt
+
 import nltk
 from nltk.tokenize import WordPunctTokenizer
 from nltk.corpus import stopwords, gutenberg
@@ -33,7 +35,7 @@ if ans == 'y':
     except FileNotFoundError:
         print("Prepared corpus not found, generating it.")
 
-if skip_generation == False:
+if skip_generation is False:
     file = open("news.csv", 'r')
     next(file)  # To skip headers row
     csv = csv.DictReader(file, delimiter=',', quotechar='"', fieldnames=['text', 'label'])
@@ -68,7 +70,7 @@ if skip_generation == False:
 
 # Limiting corpus (for faster results)
 
-prepared_corpus = prepared_corpus[:len(prepared_corpus)//4]
+prepared_corpus = prepared_corpus[:len(prepared_corpus)]
 
 # --------
 # Creating id2word dictionary
@@ -92,169 +94,168 @@ bow_corpus = [dictionary.doc2bow(text) for text in norm_corpus_bigrams]
 
 # --------
 # Task 1. Latent Dirichlet Allocation
-print("LDA Gensim:")
-Total_topics = 10
-lda_model = gensim.models.LdaModel(
-    corpus = bow_corpus,       # +
-    id2word = dictionary,      # +
-    chunksize=1416,            # +
-    alpha = 'auto',            # +
-    eta = 'auto',              # +
-    random_state = 0,          # +
-    iterations=500,            # +
-    num_topics = Total_topics, # +
-    passes = 20,               # +
-    eval_every=None            # +
-)
-
-for topic_id, topic in lda_model.print_topics(num_topics=10, num_words=20):
-    print('Topic #'+str(topic_id+1)+':')
-    print(topic)
-
-print("Coherence: ", end='')
-topics_coherences = lda_model.top_topics(bow_corpus, topn=20)
-avg_coherence_score = np.mean([item[1] for item in topics_coherences])
-print(avg_coherence_score)
-## --------
-## Analysing coherence
-cv_coherence_model_lda = gensim.models.CoherenceModel(
-    model=lda_model, corpus=bow_corpus, texts=norm_corpus_bigrams,
-    dictionary=dictionary, coherence='c_v')
-avg_coherence_cv = cv_coherence_model_lda.get_coherence()
-print("Avg. Coherence Score (CV): " + str(avg_coherence_cv))
-
-umass_coherence_model_lda = gensim.models.CoherenceModel(
-    model=lda_model, corpus=bow_corpus, texts=norm_corpus_bigrams,
-    dictionary=dictionary, coherence='u_mass')
-avg_coherence_umass = umass_coherence_model_lda.get_coherence()
-print("Avg. Coherence Score (U-Mass): " + str(avg_coherence_umass))
-
-perplexity = lda_model.log_perplexity(bow_corpus)
-print("Model Perplexity: " + str(perplexity))
-# --------
-# Comparing to other models
-# LSI Gensim
-print("LSI Gensim:")
-lsi_bow = gensim.models.LsiModel(
-    bow_corpus,
-    id2word = dictionary,
-    num_topics = Total_topics,
-    onepass = True,
-    chunksize = 1416,
-    power_iters = 500
-)
-
-for topic_id, topic in lsi_bow.print_topics(num_topics=10, num_words=20):
-    print('Topic #'+str(topic_id+1)+':')
-    print(topic)
+if 1:
+    print("LDA Gensim:")
+    Total_topics = 10
+    lda_model = gensim.models.LdaModel(
+        corpus = bow_corpus,       # +
+        id2word = dictionary,      # +
+        chunksize=1416,            # +
+        alpha = 'auto',            # +
+        eta = 'auto',              # +
+        random_state = 0,          # +
+        iterations=500,            # +
+        num_topics = Total_topics, # +
+        passes = 20,               # +
+        eval_every=None            # +
+    )
     
-## --------
-## Analysing coherence
-cv_coherence_model_lda = gensim.models.CoherenceModel(
-    model=lsi_bow, corpus=bow_corpus, texts=norm_corpus_bigrams,
-    dictionary=dictionary, coherence='c_v')
-avg_coherence_cv = cv_coherence_model_lda.get_coherence()
-print("Avg. Coherence Score (CV): " + str(avg_coherence_cv))
-
-umass_coherence_model_lda = gensim.models.CoherenceModel(
-    model=lsi_bow, corpus=bow_corpus, texts=norm_corpus_bigrams,
-    dictionary=dictionary, coherence='u_mass')
-avg_coherence_umass = umass_coherence_model_lda.get_coherence()
-print("Avg. Coherence Score (U-Mass): " + str(avg_coherence_umass))
-
-# perplexity = lsi_bow.log_perplexity(bow_corpus)
-print("Model Perplexity: N/A")
+    for topic_id, topic in lda_model.print_topics(num_topics=10, num_words=20):
+        print('Topic #'+str(topic_id+1)+':')
+        print(topic)
+    
+    print("Coherence: ", end='')
+    topics_coherences = lda_model.top_topics(bow_corpus, topn=20)
+    avg_coherence_score = np.mean([item[1] for item in topics_coherences])
+    print(avg_coherence_score)
+    ## --------
+    ## Analysing coherence
+    cv_coherence_model_lda = gensim.models.CoherenceModel(
+        model=lda_model, corpus=bow_corpus, texts=norm_corpus_bigrams,
+        dictionary=dictionary, coherence='c_v')
+    avg_coherence_cv = cv_coherence_model_lda.get_coherence()
+    print("Avg. Coherence Score (CV): " + str(avg_coherence_cv))
+    
+    umass_coherence_model_lda = gensim.models.CoherenceModel(
+        model=lda_model, corpus=bow_corpus, texts=norm_corpus_bigrams,
+        dictionary=dictionary, coherence='u_mass')
+    avg_coherence_umass = umass_coherence_model_lda.get_coherence()
+    print("Avg. Coherence Score (U-Mass): " + str(avg_coherence_umass))
+    
+    perplexity = lda_model.log_perplexity(bow_corpus)
+    print("Model Perplexity: " + str(perplexity))
 # --------
-# LSI sklearn
-print("LSI sklearn:")
-# BOW sklearn
-cv = CountVectorizer(min_df=20, max_df=0.6, ngram_range=(1,2),
-token_pattern=None, tokenizer=lambda doc: doc,
-preprocessor=lambda doc: doc)
-cv_features = cv.fit_transform(norm_papers)
-vocabulary = np.array(cv.get_feature_names_out())
+# Now compare it to other Total_topics numbers
+x = []
+y1 = []
+y2 = []
+y3 = []
+ans = input("Do you want to load already prepared corpus? (Y/N) ").lower()
+skip_generation = False
+if ans == 'y':
+    try:
+        with open("prepared_graph.txt", 'r') as file:
+            line = []
+            for l in file:
+                line.append(l.strip())
 
-lsi_model = TruncatedSVD(
-    n_components = Total_topics,
-    n_iter = 500,
-    random_state = 0
-)
-document_topics = lsi_model.fit_transform(cv_features)
+            i = 0
+            while line[i] != '---':
+                y1.append(float(line[i]))
+                i = i + 1
 
-topic_terms = lsi_model.components_
-topic_key_term_idxs = np.argsort(-np.absolute(topic_terms),axis=1)[:, :10]
-topic_keyterms = vocabulary[topic_key_term_idxs]
-topics = [', '.join(topic) for topic in topic_keyterms]
-pd.set_option('display.max_colwidth', None)
-topics_df = pd.DataFrame(
-    topics, 
-    columns = ['Terms per Topic'],
-    index=['Topic'+str(t) for t in range(1, Total_topics+1)]
-)
-print(topics_df)
+            i = i + 1
+            while line[i] != '---':
+                y2.append(float(line[i]))
+                i = i + 1
 
-# --------
-# LDA sklearn
-print("LDA sklearn:")
-lda_model2 = LatentDirichletAllocation(
-    n_components = Total_topics,
-    max_iter = 500,
-    max_doc_update_iter = 50,
-    learning_method = 'online',
-    batch_size = 1416,
-    learning_offset = 50.,
-    random_state = 0,
-    n_jobs = -1 # All cpu power
-)
-document_topics2 = lda_model2.fit_transform(cv_features)
+            i = i + 1
+            while line[i] != '---':
+                y3.append(float(line[i]))
+                i = i + 1
 
-topic_terms = lda_model2.components_
-topic_key_term_idxs = np.argsort(-np.absolute(topic_terms),axis=1)[:, :10]
-topic_keyterms = vocabulary[topic_key_term_idxs]
-topics = [', '.join(topic) for topic in topic_keyterms]
-pd.set_option('display.max_colwidth', None)
-topics_df = pd.DataFrame(
-    topics, 
-    columns = ['Terms per Topic'],
-    index=['Topic'+str(t) for t in range(1, Total_topics+1)]
-)
-print(topics_df)
+            for v in range(1, len(y1)+1):
+                x.append(v)
 
-print("LDA sklearn perplexity: " + str(lda_model2.perplexity(cv_features)))
-print("LDA sklearn score: " + str(lda_model2.score(cv_features)))
-# --------
-# NMF sklearn
-print("NMF sklearn:")
-nmf_model = NMF(
-    n_components = Total_topics,
-    solver = 'cd',
-    max_iter = 500,
-    random_state = 0,
-    # alpha_W = .1,
-    l1_ratio = .85,
-    init = "nndsvd"
-)
-document_topics3 = nmf_model.fit_transform(cv_features)
+        skip_generation = True
+    except FileNotFoundError:
+        print("Prepared corpus not found, generating it.")
 
-topic_terms = nmf_model.components_
-topic_key_term_idxs = np.argsort(-np.absolute(topic_terms),axis=1)[:, :10]
-topic_keyterms = vocabulary[topic_key_term_idxs]
-topics = [', '.join(topic) for topic in topic_keyterms]
-pd.set_option('display.max_colwidth', None)
-topics_df = pd.DataFrame(
-    topics, 
-    columns = ['Terms per Topic'],
-    index=['Topic'+str(t) for t in range(1, Total_topics+1)]
-)
-print(topics_df)
+print(x)
+print(y1)
+print(y2)
+print(y3)
+
+if skip_generation is False:
+    for i in range(1, 20 +1) :
+        print("i = " + str(i) + ":")
+        x.append(i)
+        lda_model = gensim.models.LdaModel(
+            corpus = bow_corpus,       # +
+            id2word = dictionary,      # +
+            chunksize=1416,            # +
+            alpha = 'auto',            # +
+            eta = 'auto',              # +
+            random_state = 0,          # +
+            iterations=500,            # +
+            num_topics = i,            # +
+            passes = 20,               # +
+            eval_every=None            # +
+        )
+        
+        print("Coherence: ", end='')
+        topics_coherences = lda_model.top_topics(bow_corpus, topn=20)
+        avg_coherence_score = np.mean([item[1] for item in topics_coherences])
+        print(avg_coherence_score)
+        y1.append(avg_coherence_score)
+        ## --------
+        ## Analysing coherence
+        cv_coherence_model_lda = gensim.models.CoherenceModel(
+            model=lda_model, corpus=bow_corpus, texts=norm_corpus_bigrams,
+            dictionary=dictionary, coherence='c_v')
+        avg_coherence_cv = cv_coherence_model_lda.get_coherence()
+        print("Avg. Coherence Score (CV): " + str(avg_coherence_cv))
+        y2.append(avg_coherence_cv)
+    
+        umass_coherence_model_lda = gensim.models.CoherenceModel(
+            model=lda_model, corpus=bow_corpus, texts=norm_corpus_bigrams,
+            dictionary=dictionary, coherence='u_mass')
+        avg_coherence_umass = umass_coherence_model_lda.get_coherence()
+        print("Avg. Coherence Score (U-Mass): " + str(avg_coherence_umass))
+        y3.append(avg_coherence_umass)
+    
+        perplexity = lda_model.log_perplexity(bow_corpus)
+        print("Model Perplexity: " + str(perplexity))
+            
+    with open("prepared_graph.txt", 'w') as file2:
+        for y in y1:
+            file2.write(str(y) + '\n')
+        file2.write("---\n")
+        for y in y2:
+            file2.write(str(y) + '\n')
+        file2.write("---\n")
+        for y in y3:
+            file2.write(str(y) + '\n')
+        file2.write("---\n")
+
+fig, axs = plt.subplots(2)
+axs[0].plot(x, y2, marker = 'o')
+axs[1].plot(x, y3, marker = 'o')
+plt.show()
 
 # --------
 # Task 2. NLTK Gutenberg Trigrams
 print("Gutenberg trigrams:")
 
 gutenberg_sents = gutenberg.sents(fileids="chesterton-thursday.txt")
+gutenberg_sents = [' '.join(gutenberg_sents[i]) for i in range(len(gutenberg_sents))]
 
-finder = TrigramCollocationFinder.from_documents(gutenberg_sents)
+prepared_corpus = []
+wpt = WordPunctTokenizer()
+
+def preprocess_sentence(sentence):
+    sentence = re.sub(r'[^a-zA-Z\s]+', '', sentence, re.I | re.A)
+    sentence = sentence.lower()
+    sentence = sentence.strip()
+    tokens = wpt.tokenize(sentence)
+    filtered_tokens = [token for token in tokens if token not in stopwords.words('english')]
+    sentence = filtered_tokens
+    return sentence
+
+for sentence in gutenberg_sents:
+    prepared_corpus.append(preprocess_sentence(sentence))
+
+finder = TrigramCollocationFinder.from_documents(prepared_corpus)
 trigram_measures = TrigramAssocMeasures()
 
 print("Top 10 raw frequency: ", end='')
